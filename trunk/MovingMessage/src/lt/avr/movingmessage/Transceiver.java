d8278aba8bb0b8cd12c1c4ba7eb758ad07f283c7
baioraz <baioraz@dde5b427-57a4-2db7-af85-145f9adcb7a0>
2010-12-07 19:03:53 +0000

    one way

--- trunk/MovingMessage/src/lt/avr/movingmessage/Transceiver.java
+++ trunk/MovingMessage/src/lt/avr/movingmessage/Transceiver.java
@@ -17,21 +17,10 @@ public class Transceiver extends TimerTask {
 		Transceiver transceiver = new Transceiver();
 
 		if (args.length > 1) {
-			debugEnabled = true;
-			if (args[0].equals("readConfigurationData")) {
-				readConfigurationData();
-			} else if (args[0].equals("writeConfigurationData")) {
-				if (args.length == 3) {
-					writeConfigurationData(10, args[1], args[2], "1");
-				} else {
-					System.out.println("wrong parameters");
-				}
-			} else if (args[0].equals("sendData")) {
+
 				Timer timer = new Timer();
 				timer.schedule(transceiver, 1, 1000 * 60 * 2);
-			} else {
-				System.out.println("wrong operation");
-			}
+
 		} else {
 			System.out.println("no operation");
 		}
@@ -52,9 +41,7 @@ public class Transceiver extends TimerTask {
 
 	public static void sendData(String data) {
 		try {
-			if (debugEnabled) {
-				System.out.println("TRANSMITTING DATA");
-			}
+			System.out.println("TRANSMITTING DATA");
 
 			JD2XX transceiver = new JD2XX();
 			setUpParameters(transceiver, 10000, 10000);
@@ -65,47 +52,12 @@ public class Transceiver extends TimerTask {
 			System.out.println(numberOfPackets);
 			byte controlPacketTransmitStatus = transmitControlPacket(transceiver, (numberOfPackets < 128) ? numberOfPackets : 128, Constants.OPERATION_PUSH__DATA);
 			if (controlPacketTransmitStatus == 1) {
-				Thread.sleep(500);
+				Thread.sleep(2000);
 				transmitData(transceiver, data, numberOfPackets);
-				Thread.sleep(500);
-				byte[] controlPacket = receiveControlPacket(transceiver);
 				byte[] singleByteBuffer = new byte[1];
 				transceiver.read(singleByteBuffer);
-				if (debugEnabled) {
-					System.out.println("Transaction complete status: " + singleByteBuffer[0]);
-				}
-			} else {
-				System.out.println("Transaction failed");
-			}
-			closeTransceiver(transceiver);
+				System.out.println("Transaction complete status: " + singleByteBuffer[0]);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public static void writeConfigurationData(int scrollSpeed, String rssWeatherUrl, String rssNewsUrl, String pattern) {
-		try {
-			if (debugEnabled) {
-				System.out.println("TRANSMITTING DATA");
-			}
-
-			JD2XX transceiver = new JD2XX();
-			setUpParameters(transceiver, 10000, 10000);
-			byte controlPacketTransmitStatus = transmitControlPacket(transceiver, 16, Constants.OPERATION_SAVE_CONFIGUTATION_DATA);
-			if (controlPacketTransmitStatus == 1) {
-				Thread.sleep(500);
-				sendConfigurationData(transceiver, scrollSpeed, 100, 1, rssWeatherUrl, rssNewsUrl, pattern);
-				Thread.sleep(500);
-				byte[] controlPacket = receiveControlPacket(transceiver);
-				byte[] dataBytes = receiveData(transceiver, controlPacket);
-				byte[] singleByteBuffer = new byte[1];
-				transceiver.read(singleByteBuffer);
-				if (debugEnabled) {
-					System.out.println("Transaction complete status: " + singleByteBuffer[0]);
-				}
-				printDataBytes(dataBytes);
 			} else {
 				System.out.println("Transaction failed");
 			}
@@ -118,9 +70,7 @@ public class Transceiver extends TimerTask {
 	}
 
 	private static void transmitData(JD2XX transceiver, String data, int numberOfPackets) throws IOException, InterruptedException {
-		if (debugEnabled) {
-			System.out.println("Sending data bytes");
-		}
+		System.out.println("Sending data bytes");
 
 		byte[] packetData = new byte[2048];
 		System.out.println(packetData.length);
@@ -130,84 +80,14 @@ public class Transceiver extends TimerTask {
 		int start = 0;
 		for (int i = 0; i < numberOfPackets; i++) {
 			transceiver.write(packetData, start, 16);
-			Thread.sleep(250);
+			Thread.sleep(300);
 			int b = transceiver.read();
-			if (debugEnabled) {
-				System.out.println("Data packet transmission status: " + b);
-			}
+			System.out.println("Data packet transmission status: " + b);
 			start+=16;
 		}
 		
-		if (debugEnabled) {
-			System.out.println("Data bytes sent");
-		}
-	}
+		System.out.println("Data bytes sent");
 
-	public static void readConfigurationData() {
-		try {
-			if (debugEnabled) {
-				System.out.println("TRANSMITTING DATA");
-			}
-
-			JD2XX transceiver = new JD2XX();
-			setUpParameters(transceiver, 10000, 10000);
-			byte controlPacketTransmitStatus = transmitControlPacket(transceiver, 0, Constants.OPERATION_READ_CONFIGUTATION_DATA);
-			if (controlPacketTransmitStatus == 1) {
-				// Thread.sleep(1000);
-				byte[] controlPacket = receiveControlPacket(transceiver);
-				byte[] dataBytes = receiveData(transceiver, controlPacket);
-				byte[] singleByteBuffer = new byte[1];
-				transceiver.read(singleByteBuffer);
-				if (debugEnabled) {
-					System.out.println("Transaction complete status: " + singleByteBuffer[0]);
-				}
-				printDataBytes(dataBytes);
-			} else {
-				System.out.println("Transaction failed");
-			}
-			closeTransceiver(transceiver);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Error");
-			System.out.println(e.getMessage());
-		}
-	}
-
-	private static void sendConfigurationData(JD2XX transceiver, int scrollSpeed, int receiveTimeout, int deviceDebugEnabled, String rssWeatherUrl,
-			String rssNewsUrl, String pattern) throws IOException, InterruptedException {
-		if (debugEnabled) {
-			System.out.println("Sending config data bytes");
-		}
-
-		byte[] packetData = new byte[256];
-
-		// First packet of data
-		packetData[0] = (byte) scrollSpeed;
-		packetData[1] = (byte) receiveTimeout;
-		packetData[2] = (byte) deviceDebugEnabled;
-
-		// Second packet of data (currently empty)
-
-		// Copy 6 bytes of rss weather url starting from position - 32
-		System.arraycopy(rssWeatherUrl.getBytes(), 0, packetData, 32, rssWeatherUrl.getBytes().length);
-		// copy 6 bytes of rss news url starting from position - 128
-		System.arraycopy(rssNewsUrl.getBytes(), 0, packetData, 128, rssNewsUrl.getBytes().length);
-		// copy 2 pattern bytes starting from position - 224
-		System.arraycopy(pattern.getBytes(), 0, packetData, 224, pattern.getBytes().length);
-		for (int i = 1; i < packetData.length + 1; i++) {
-			transceiver.write(packetData[i - 1]);
-			if ((i % 16) == 0) {
-				Thread.sleep(220);
-				int b = transceiver.read();
-				if (debugEnabled) {
-					System.out.println("Data packet transmission status: " + b);
-				}
-			}
-		}
-		if (debugEnabled) {
-			System.out.println("Data bytes sent");
-		}
 	}
 
 	private static void printDataBytes(byte[] dataBytes) {
@@ -216,100 +96,20 @@ public class Transceiver extends TimerTask {
 		}
 	}
 
-	private static byte[] receiveData(JD2XX transceiver, byte[] controlPacket) throws IOException {
-		if (debugEnabled) {
-			System.out.println("RECEIVING DATA");
-		}
-
-		int numberOfPacketsToReceive = controlPacket[0];
-		StringBuffer status = new StringBuffer();
-		status.append((char) controlPacket[1]);
-		status.append((char) controlPacket[2]);
-
-		if ("OK".equals(status.toString())) {
-			return receiveDataPackets(transceiver, numberOfPacketsToReceive);
-		} else {
-			return "Transaction failed".getBytes();
-		}
-
-	}
-
-	private static byte[] receiveControlPacket(JD2XX transceiver) throws IOException {
-		if (debugEnabled) {
-			System.out.println("Receiving control packet");
-		}
-		byte[] singleByteBuffer = new byte[1];
-		singleByteBuffer[0] = 0;
-
-		// Read reception status
-		transceiver.read(singleByteBuffer);
-		if (debugEnabled) {
-			System.out.println("reception status: " + singleByteBuffer[0]);
-		}
-		// Read config data packet
-		byte[] packet = new byte[16];
-		transceiver.read(packet);
-		if (debugEnabled) {
-			for (int i = 0; i < packet.length; i++) {
-				System.out.println("Control data byte [" + i + "]:" + packet[i]);
-			}
-		}
-		return packet;
-	}
-
-	private static byte[] receiveDataPackets(JD2XX transceiver, int numberOfPacketsToReceive) throws IOException {
-		byte[] singleByteBuffer = new byte[1];
-		if (debugEnabled) {
-			System.out.println("Receiving data bytes");
-		}
-
-		byte[] tmpBuf = new byte[16 * numberOfPacketsToReceive];
-		int bytesRead = 0;
-		int start = 0;
-		for (int i = 0; i < numberOfPacketsToReceive; i++) {
-			transceiver.read(singleByteBuffer);
-			if (debugEnabled) {
-				System.out.println("Reception status [" + i + "]:" + singleByteBuffer[0]);
-			}
-
-			byte readBytes[] = transceiver.read(16);
-			if (debugEnabled) {
-				for (int j = 0; j < readBytes.length; j++) {
-					System.out.println("Received data byte [" + j + "]:" + (char) readBytes[j]);
-				}
-			}
-			bytesRead = bytesRead + readBytes.length;
-			System.arraycopy(readBytes, 0, tmpBuf, start, readBytes.length);
-			start = start + 16;
-		}
-
-		byte[] dataBytes = new byte[bytesRead];
-		System.arraycopy(tmpBuf, 0, dataBytes, 0, bytesRead);
-
-		return dataBytes;
-	}
-
 	private static byte transmitControlPacket(JD2XX transceiver, int numberOfPackets, int operation) throws IOException, InterruptedException {
 		// Control packet
 		byte[] packet = new byte[16];
 		packet[0] = (byte) numberOfPackets;
-		packet[1] = (byte) operation;
 		transceiver.write(packet);
 
-		// Thread.sleep(500);
-		if (debugEnabled) {
-			System.out.println("Control packet sent");
-		}
+		System.out.println("Control packet sent");
+
 		byte singleByteBuffer[] = new byte[1];
 		// Read fifo status
 		transceiver.read(singleByteBuffer);
-		if (debugEnabled) {
-			System.out.println("FIFO receive status: " + singleByteBuffer[0]);
-		}
+		System.out.println("FIFO receive status: " + singleByteBuffer[0]);
 		transceiver.read(singleByteBuffer);
-		if (debugEnabled) {
-			System.out.println("Control packet transmission status: " + singleByteBuffer[0]);
-		}
+		System.out.println("Control packet transmission status: " + singleByteBuffer[0]);
 		return singleByteBuffer[0];
 	}
 
