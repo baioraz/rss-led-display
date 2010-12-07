45affb08adf860eb3f10e84b12382c8a1a3804ca
baioraz <baioraz@dde5b427-57a4-2db7-af85-145f9adcb7a0>
2010-12-07 18:56:17 +0000

    removed two way communication

--- trunk/LEDMatrix/src/global.c
+++ trunk/LEDMatrix/src/global.c
@@ -1,5 +1,6 @@
 unsigned char packetReceiveTimeout;
 unsigned char rssi = 0;
 unsigned char message_buffer[80];
+unsigned char configuration[16];
 unsigned char FALSE = 0;
 unsigned char TRUE = 1;
--- trunk/LEDMatrix/src/global.h
+++ trunk/LEDMatrix/src/global.h
@@ -4,6 +4,18 @@
 extern unsigned char packetReceiveTimeout;
 extern unsigned char rssi;
 extern unsigned char message_buffer[80];
+extern unsigned char configuration[16];
+/*
+ * [0] = numberOfPackets
+ * [1] = time
+ * [2] = time
+ * [3] = time
+ * [4] = time
+ * [5] = time
+ * [6] = time
+ * [7] = speed
+ * ...
+ */
 extern unsigned char FALSE;
 extern unsigned char TRUE;
 
